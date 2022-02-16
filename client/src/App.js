7d192800c0f2a000a6f53ad5677bd33da14f6554
Thibault Carayon <thibault@maarkt.io>
2022-02-16 11:32:12 +0100

    make routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 import './App.css'
-import Register from './components/Register'
+import ApolloProvider from './ApolloProvider';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 
-function App() {
+function App(props) {
   return (
-    <Container className="pt-5">
-      <Register />
-    </Container>
+    <ApolloProvider>
+      <BrowserRouter>
+        <Container className="pt-5">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
