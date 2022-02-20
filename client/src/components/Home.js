c2bef9b92d565ea344bf5dca602e90d01b2a9cb5
Thibault Carayon <thibault@maarkt.io>
2022-02-21 00:01:13 +0100

    add home component with logout, create event and register button, redirect and logout correctly

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Row, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import EventList from './EventList';
+
+import { useAuthDispatch } from '../context/auth';
 
 export default function Home() {
+  const dispatch = useAuthDispatch();
+
+  let navigate = useNavigate();
+
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' })
+    navigate('/login')
+  }
+
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Fragment>
+        <Row className="bg-white justify-content-around mb-1">
+          <Link to="/login">
+            <Button variant="link">Login</Button>
+          </Link>
+          <Link to="/register">
+            <Button variant="link">Register</Button>
+          </Link>
+          <Link to="/create-event">
+            <Button variant="link">Create an event</Button>
+          </Link>
+          <Button variant="link" onClick={logout}>
+            Logout
+          </Button>
+        </Row>
+        <EventList />
+  </Fragment>
   )
 }
