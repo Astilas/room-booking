0adfb606f41b6b0c113aefb8b55e99778851c708
Thibault Carayon <thibault@maarkt.io>
2022-02-21 00:10:01 +0100

    update app.js add some routes, providers

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,47 @@ import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
 
+import CreateEvent from './components/CreateEvent';
+import UpdateEvent from './components/UpdateEvent';
+
+import { AuthProvider } from './context/auth';
+import { EventProvider } from './context/events';
+import { RoomProvider } from './context/rooms';
+import ProtectedRoute from './components/ProtectedRoute';
+import GuestRoute from './components/GuestRoute';
+// import Event from './components/Event';
+
 function App(props) {
   return (
     <ApolloProvider>
-      <BrowserRouter>
-        <Container className="pt-5">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Container>
-      </BrowserRouter>
+      <AuthProvider>
+        <EventProvider>
+          <RoomProvider>
+            <BrowserRouter>
+              <Container className="pt-5">
+                <Routes>
+                  <Route exact patho='/' element={<ProtectedRoute/>} >
+                    <Route exact path='/' element={<Home />} />
+                  </Route>
+                  <Route exact path='/' element={<ProtectedRoute/>} >
+                    <Route exact path='/create-event' element={<CreateEvent />} />
+                  </Route>
+                  <Route exact path='/' element={<ProtectedRoute/>} >
+                    <Route exact path='/update-event/:id' element={<UpdateEvent />} />
+                  </Route>
+                  {/* <Route exact path='/' element={<ProtectedRoute/>} >
+                    <Route exact path='/event/:id' element={<Event />} />
+                  </Route> */}
+                  <Route exact path='/' element={<GuestRoute/>} >
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                  </Route>
+                </Routes>
+              </Container>
+            </BrowserRouter>
+          </RoomProvider>
+        </EventProvider>
+      </AuthProvider>
     </ApolloProvider>
   )
 }
