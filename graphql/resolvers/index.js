9b336c77494ec62d8793eefd4ae2c023880b12a9
Thibault Carayon <thibault@maarkt.io>
2022-02-20 20:16:20 +0100

    add association room_id and user_id to event table

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,8 +1,14 @@
-const userResolvers = require('./users')
-const roomResolvers = require('./rooms')
-const eventResolvers = require('./events')
+const userResolvers = require('./users');
+const roomResolvers = require('./rooms');
+const eventResolvers = require('./events');
+const { User, Room } = require('../../models');
 
 module.exports = {
+  Event: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+    room_id: async (parent) => await Room.findByPk(parent.room_id),
+    user_id: async (parent) => await User.findByPk(parent.user_id),
+  },
   Query: {
     ...userResolvers.Query,
     ...roomResolvers.Query,
--- models/Event.js
+++ models/Event.js
@@ -9,8 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ User, Room }) {
       // define association here
+      this.belongsTo(Room, { foreignKey: 'room_id' })
+      this.belongsTo(User, { foreignKey: 'user_id' })
     }
   }
   Event.init({
@@ -18,12 +20,30 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING(50),
       allowNull: false,
     },
-    description: DataTypes.STRING,
-    date: DataTypes.STRING,
-    begin_hour: DataTypes.STRING,
-    end_hour: DataTypes.STRING,
-    user_id: DataTypes.INTEGER,
-    room_id: DataTypes.INTEGER
+    description: {
+      type: DataTypes.STRING(300),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    begin_hour: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    end_hour: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    room_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   }, {
     sequelize,
     // disable the modification of table names; By default, sequelize will automatically
