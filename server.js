ba61227204e15f46c5e61266b51c993e0c509ca0
Thibault Carayon <thibault@maarkt.io>
2022-02-22 21:58:22 +0100

    update server.js ==> set up subscription + build context directly on this file (for now)

--- server.js
+++ server.js
@@ -1,33 +1,79 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
 const { sequelize } = require('./models');
-const cors = require('cors');
+const { createServer } = require('http');
+const { execute, subscribe }  = require('graphql');
+const { SubscriptionServer }  = require('subscriptions-transport-ws');
+const { makeExecutableSchema }  = require('@graphql-tools/schema');;
 
-const resolvers = require('./graphql/resolvers')
-const typeDefs = require('./graphql/typeDefs')
-const contextMiddleware = require('./util/contextMiddleware')
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+// const contextMiddleware = require('./util/contextMiddleware');
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-  };
+const { PubSub } = require('graphql-subscriptions');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('./config/env.json');
+const pubsub = new PubSub();
 
-const server = new ApolloServer({
+(async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = createServer(app);
+
+  const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
-    cors: cors(corsOptions),
-    context: contextMiddleware
-  })
+  });
 
+  const server = new ApolloServer({
+    schema,
+    // context for the mutation
+    context: (context) => {
+      let token;
+      if (context.req && context.req.headers.authorization) {
+        token = context.req.headers.authorization.split('Bearer ')[1];
+      }
+
+      if (token) {
+        jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+          context.user = decodedToken;
+        });
+      }
+      context.pubsub = pubsub;
+
+      return context;
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
   
-const app = express();
+  const subscriptionServer = SubscriptionServer.create({
+    // This is the `schema` we just created.
+    schema,
+    // These are imported from `graphql`.
+    execute,
+    subscribe,
+    async onConnect(connectionParams, webSocket, context) {
+      console.log('Connected');
+      // context for subscription
+      return { pubsub };
+    },
+    async onDisconnect(websocket){
+      console.log('Disconnected');
+    }
+  }, {
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // This `server` is the instance returned from `new ApolloServer`.
+    path: server.graphqlPath,
+  });
 
-server.start().then(res => {
-    server.applyMiddleware({ app });
-    app.listen({ port: 4000 }, () => 
-        console.log(`🚀  Server ready at ${server.graphqlPath}`),
-        sequelize.authenticate()
+  await server.start();
+  server.applyMiddleware({ app });
+
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀  Server ready at http://localhost:4000${server.graphqlPath}`);
+  sequelize.authenticate()
             .then(()=> console.log('Database connected !'))
             .catch((error) => console.log(error))
-    );
-});
+})(typeDefs, resolvers);
+
