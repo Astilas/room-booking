7d4e600919600ba673e4fce31965a69a8a81f9de
Thibault Carayon <thibault@maarkt.io>
2022-02-22 21:53:36 +0100

    add useEffect when getting events + pass id to eventCard component

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import { gql, useQuery } from '@apollo/client';
 import EventCard from './EventCard';
 
@@ -24,17 +23,20 @@ export default function EventList() {
   const dispatchEvent = useEventDispatch();
   const { events } = useEventState();
 
-  let navigate = useNavigate();
+  const { loading: eventsLoading, data: eventsData } = useQuery(GET_EVENTS)
 
-  const { loading } = useQuery(GET_EVENTS, {
-    onCompleted: (data) => {
-        dispatchEvent({ type: 'GET_EVENTS', payload: data.getEvents })
-    },
-    onError: (err) => console.log("error", err),
-  })
+  useEffect(() => {
+    if (eventsData) {
+      dispatchEvent({
+        type: 'GET_EVENTS',
+        payload: eventsData.getEvents
+      })
+    }
+  }, [eventsData])
+
 
   let eventsMarkup;
-  if (!events || loading) {
+  if (!events|| eventsLoading) {
     eventsMarkup = <p>Loading..</p>
   } else if (events.length === 0) {
     eventsMarkup = <p>No events yet</p>
@@ -43,22 +45,19 @@ export default function EventList() {
         <Col lg={5} md={12} sm={12} xs={9} className="margin-event" key={event.id}>
             <EventCard
                 key={event.id}
+                id={event.id}
                 title={event.title}
                 description={event.description}
                 date={event.date}
                 begin_hour={event.begin_hour}
                 end_hour={event.end_hour}
             />
-            <Button onClick={() =>
-                navigate(`/update-event/${event.id}`)}>
-                Check event
-            </Button>
         </Col>
     ))
   }
   return (
       <Row className="bg-white">
-        <Col xs={4}>{eventsMarkup}</Col>
+        {eventsMarkup}
       </Row>
   )
 }
