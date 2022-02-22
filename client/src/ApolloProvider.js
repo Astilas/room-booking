85bbc73654b6c8d9c5161e3304cfded11eea59dc
Thibault Carayon <thibault@maarkt.io>
2022-02-22 21:56:30 +0100

    set up websocket for apollo client

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,16 +1,54 @@
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider as Provider,
-    useQuery,
-    gql
-  } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, concat, split, ApolloProvider as Provider } from "@apollo/client";
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+import { setContext } from '@apollo/client/link/context';
+
+let httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
 
 
+httpLink = concat(authLink, httpLink);
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  },
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+  
   const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache()
-  });
+    link: splitLink,
+    cache: new InMemoryCache(),
+  })
 
   export default function ApolloProvider(props){
     return <Provider client={client} {...props} />
