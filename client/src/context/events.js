449d905d0ec804ce1af814425b2df532b5374b2c
Thibault Carayon <thibault@maarkt.io>
2022-02-22 21:47:43 +0100

    add actions add and delete event on event reducer

--- client/src/context/events.js
+++ client/src/context/events.js
@@ -4,11 +4,29 @@ const EventStateContext = createContext()
 const EventDispatchContext = createContext()
 
 const eventReducer = (state, action) => {
+  let eventsCopy;
+  const { event } = action.payload
   switch (action.type) {
     case 'GET_EVENTS':
         return {
           ...state,
           events: action.payload,
+        }
+    case 'ADD_EVENT':
+      eventsCopy = [...state.events]
+        return {
+          ...state,
+          events: [...eventsCopy, event]
+        }
+    case 'DELETE_EVENT':
+      eventsCopy = [...state.events]
+      // Remove event by filter id on the event list
+      const newEventList = eventsCopy
+            .filter((e) => e.id !== event.id);
+
+        return {
+          ...state,
+          events: newEventList,
         }
     default:
       throw new Error(`Unknown action type: ${action.type}`)
