158388134f5e6e6b9475037a76cb1de5ee629377
Thibault Carayon <thibault@maarkt.io>
2022-02-27 19:50:44 +0100

    add on room resolvers subscription and mutation updateRoom

--- graphql/resolvers/rooms.js
+++ graphql/resolvers/rooms.js
@@ -1,14 +1,19 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Room } = require('../../models')
+const { Room } = require('../../models');
+const { Op, Sequelize } = require('sequelize');
+const { withFilter } = require('graphql-subscriptions');
 
 module.exports = {
     Query: {
         getRooms: async (_, __, { user }) => {
             try {
-                // if (!user) throw new AuthenticationError('Unauthenticated');
+                if (!user) throw new AuthenticationError('Unauthenticated');
 
                 const rooms = await Room.findAll({
-                    where: { availability: true }
+                    // where: Sequelize.where(
+                    //     Sequelize.fn('array_length', Sequelize.col('availability'), 1),
+                    //     { [Op.gt]: 0 },
+                    // )
                 });
         
                 return rooms;
@@ -17,5 +22,48 @@ module.exports = {
                 throw err;
             }
           },
+    },
+    Mutation: {
+        updateRoom: async (_, args, { pubsub, user }) => {
+            let { id, availability } = args;
+            try {
+                if (!user) throw new AuthenticationError('Unauthenticated');
+                const room = await Room.findOne( { where: { id: id }});
+
+                if (!room) throw new UserInputError(`Couldn’t find room with id ${id}`);
+
+                await Room.update({
+                    id,
+                    availability
+                  }, { where :{ id: id} });
+
+                const updatedRoom = await Room.findOne( { where: { id: id }});
+                pubsub.publish('CHANGE_ROOM_AVAILABILITY', { changeRoomAvailability: updatedRoom });
+
+                  // Return room
+                return room;
+
+
+            } catch (err) {
+                console.log(err);
+                throw err;
+            }
+        }
+    },
+    Subscription: {
+        changeRoomAvailability: {
+          subscribe: withFilter(
+            (_, __, { pubsub, user }) => {
+                if (!user) throw new AuthenticationError('Unauthenticated')
+                return pubsub.asyncIterator(['CHANGE_ROOM_AVAILABILITY'])
+            },
+            ( { changeRoomAvailability }, variables )=> {
+                // Only push an update if the comment is on
+                // the correct repository for this operation
+                if (changeRoomAvailability) return true
+                return false
+              },
+          )
+        },
     }
 }
