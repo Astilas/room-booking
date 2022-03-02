5775e691c11e27301da8984caf33875da72d0f2f
Thibault Carayon <thibault@maarkt.io>
2022-03-02 17:12:49 +0100

    add if statement on context

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ const pubsub = new PubSub();
       let token;
       if (context.req && context.req.headers.authorization) {
         token = context.req.headers.authorization.split('Bearer ')[1];
+      } else if (context.connection && context.connection.context.Authorization) {
+        token = context.connection.context.Authorization.split('Bearer ')[1];
       }
 
       if (token) {
