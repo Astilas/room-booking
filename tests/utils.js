7a2fa54b185f37b6004f400c5fbc83dcffbff5fa
Thibault Carayon <thibault@maarkt.io>
2022-03-08 10:16:07 +0100

    test apolloClient

--- tests/utils.js
+++ tests/utils.js
@@ -1,6 +1,6 @@
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  onError: (e) => { console.log(e) },
-});
+    uri: 'http://localhost:4000/graphql',
+    // onError: (e) => { console.log(e) },
+  })
